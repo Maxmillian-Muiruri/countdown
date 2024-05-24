74e268e33a8e3efba0692900cbb0130dd1ee0dd9
Maxmillian Muiruri <maxmillianmuiruri@gmail.com>
2024-05-24 17:55:21 +0300

    declare date

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 
 // months are ZERO index based;
-// const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 
-let futureDate = new Date(2020, 3, 24, 11, 30, 0);
+// let futureDate = new Date(2020, 3, 24, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
